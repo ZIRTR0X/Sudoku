5288690eab618f45ed3e35a3f7abbd17ac1ad024
ZIRTR0X <76250995+ZIRTR0X@users.noreply.github.com>
2022-09-02 11:51:23 +0200

    Adding help: highlighting rows, columns and groups

--- public/js/gridBox.js
+++ public/js/gridBox.js
@@ -4,9 +4,40 @@ function unselectBoxes(boxe){
     boxe.forEach(function(box){
         if(box.classList.contains("selected")){
             box.classList.remove("selected");
-            box.classList.add("unselected");
         }
+        if(box.classList.contains("colorhelp")){
+            box.classList.remove("colorhelp");
+        }
+        box.classList.add("unselected");
+        
     }); 
+}
+
+function rowSelectBoxes(row){
+    boxe.forEach(function(box){
+        if(box.classList.contains(row) && !box.classList.contains("selected")){
+            box.classList.remove("unselected");
+            box.classList.add("colorhelp");
+        }
+    });
+}
+
+function columnSelectBoxes(column){
+    boxe.forEach(function(box){
+        if(box.classList.contains(column) && !box.classList.contains("selected")){
+            box.classList.remove("unselected");
+            box.classList.add("colorhelp");
+        }
+    });
+}
+
+function groupSelectBoxes(group){
+    boxe.forEach(function(box){
+        if(box.classList.contains(group) && !box.classList.contains("selected")){
+            box.classList.remove("unselected");
+            box.classList.add("colorhelp");
+        }
+    });
 }
 
 boxe.forEach(function(box){
@@ -14,5 +45,11 @@ boxe.forEach(function(box){
         unselectBoxes(boxe);
         box.classList.remove("unselected");
         box.classList.add("selected");
+        let row = box.classList[0];
+        let group = box.classList[1];
+        let column = box.classList[2];
+        rowSelectBoxes(row);
+        groupSelectBoxes(group);
+        columnSelectBoxes(column);
     });
 });
